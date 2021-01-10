c8cddf9a4ae8725101386f68492f4b2ec6bc4ff9
fulder <misad90@gmail.com>
2021-01-10 12:32:38 +0100

    Create moshan item function for all needed html fields

--- includes/js/api/common.js
+++ includes/js/api/common.js
@@ -11,3 +11,16 @@ async function axiosTokenInterceptor (config) {
   config.headers.Authorization = accessToken;
   return config;
 }
+
+/* exported MoshanItem */
+function MoshanItem(  airing,  title,  start_date,  status,  synopsis,  mal_link,  anidb_link) {
+  this.poster = poster;
+  this.title = title;
+  this.start_date = start_date;
+  this.status = status;
+  this.synopsis = synopsis;
+
+  // TODO: make links more dynamic
+  this.mal_link = mal_link;
+  this.anidb_link = anidb_link;
+}
