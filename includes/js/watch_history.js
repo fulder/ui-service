5bfe9537f1c64c50ff860b141586dfad15cd5674
fulder <misad90@gmail.com>
2021-01-16 12:32:32 +0100

    Move spinner creation to main collection loop

--- includes/js/watch_history.js
+++ includes/js/watch_history.js
@@ -12,14 +12,14 @@ if (accessToken === null) {
   document.getElementById('logInAlert').className = 'alert alert-danger';
 } else {
   document.getElementById('logInAlert').className = 'd-none';
-  document.getElementById('animeWatchHistory').innerHTML = '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>';
-  document.getElementById('showsWatchHistory').innerHTML = '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>';
 }
 
 createCollections();
 
 async function createCollections() {
   for (var collectionName in collectionNames) {
+    document.getElementById(`${collectionName}WatchHistory`).innerHTML = '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>';
+
     const res = await watchHistoryApi.getWatchHistoryByCollection(collectionName);
     await createItems(res.data, collectionName);
   }
