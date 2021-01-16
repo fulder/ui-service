932ebfe86520afa1d17e786692aaf3b04a37e527
fulder <misad90@gmail.com>
2021-01-16 13:07:45 +0100

    Add debug log for anime item

--- includes/js/api/mal.js
+++ includes/js/api/mal.js
@@ -33,6 +33,7 @@ class MalApi {
   }
 
   getMoshanItem(anime) {
+    console.debug(anime);
     let status = 'Airing';
     if ('end_date' in anime && anime.end_date !== null) {
       status = 'Finished';
