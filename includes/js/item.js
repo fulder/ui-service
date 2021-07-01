82e660a108e0276cbdd38a1cbb3b6900ad6b0f4c
fulder <misad90@gmail.com>
2021-07-02 01:05:42 +0200

    Revert back to episode_id again

--- includes/js/item.js
+++ includes/js/item.js
@@ -151,7 +151,7 @@ function createEpisodesList (moshanEpisodes) {
 
     if (episode.aired && qParams.id !== null) {
       rowClass = 'episodeRow';
-      onClickAction = `window.location='/episode?collection=${qParams.collection}&id=${qParams.id}&api_name=${qParams.api_name}&api_id=${episode.id}'`;
+      onClickAction = `window.location='/episode?collection=${qParams.collection}&id=${qParams.id}&api_name=${qParams.api_name}&episode_id=${episode.id}'`;
     }
 
     tableHTML += `
