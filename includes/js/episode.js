4d4602bcdd86aa544033da0c4b86b722bc6ef45a
fulder <misad90@gmail.com>
2021-07-02 23:22:11 +0200

    Support episode by id and api_id

--- includes/js/episode.js
+++ includes/js/episode.js
@@ -23,7 +23,13 @@ function QueryParams(urlParams) {
 async function getEpisode() {
   let watchHistoryEpisode = null;
   try {
-    const watchHistoryRes = await watchHistoryApi.getWatchHistoryEpisodeByApiId(qParams);
+    let watchHistoryRes = null;
+    if (qParams.api_id !== null) {
+        watchHistoryRes = await watchHistoryApi.getWatchHistoryEpisodeByApiId(qParams);
+    } else if (qParams.episode_id !== null) {
+        watchHistoryRes = await watchHistoryApi.getWatchHistoryEpisode(qParams);
+    }
+
     watchHistoryEpisode = watchHistoryRes.data;
     qParams.api_id = watchHistoryEpisode[`${qParams.api_name}_id`];
   } catch(error) {
